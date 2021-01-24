93d3b36384c48ad26e63a4fd5eb60b491993bde8
Jazib Dawre <jazib980@gmail.com>
2021-01-24 20:17:05 +0530

    Add resolvers for orders

--- backend/graphql/resolvers/index.js
+++ backend/graphql/resolvers/index.js
@@ -1,3 +1,17 @@
-module.exports = {
-  //resolvers here
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getMyOrders,
+  getOrders,
+} from './order.js';
+
+export default {
+  orders: getOrders,
+  myorders: getMyOrders,
+  orderById: getOrderById,
+  createOrder: addOrderItems,
+  updateOrderToPaid: updateOrderToPaid,
+  updateOrderToDelivered: updateOrderToDelivered,
 };
