c611bdb306a63b7ab166f046a62f0032cb8d0afb
Jazib Dawre <jazib980@gmail.com>
2021-01-26 17:36:54 +0530

    Add resolvers to index, minor changes

--- backend/graphql/resolvers/index.js
+++ backend/graphql/resolvers/index.js
@@ -1,4 +1,15 @@
 import {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from './user.js';
+
+import {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
@@ -7,20 +18,27 @@ import {
   getOrders,
 } from './order.js';
 
-import { 
-  questions,
-  question,
-  editQuestions,
- } from './chatbot.js';
+import { questions, question, editQuestions } from './chatbot.js';
 
 export default {
+  //orders
   orders: getOrders,
   myorders: getMyOrders,
   orderById: getOrderById,
   createOrder: addOrderItems,
   updateOrderToPaid: updateOrderToPaid,
   updateOrderToDelivered: updateOrderToDelivered,
+  //chatbot
   questions: questions,
   question: question,
   editQuestions: editQuestions,
+  //users
+  authUser: authUser,
+  registerUser: registerUser,
+  getUserProfile: getUserProfile,
+  updateUserProfile: updateUserProfile,
+  getUsers: getUsers,
+  deleteUser: deleteUser,
+  getUserById: getUserById,
+  updateUser: updateUser,
 };
