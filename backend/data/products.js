f99a661da104020ec88b18a31d302d166c6eca78
Jazib Dawre <jazib980@gmail.com>
2021-02-01 00:09:23 +0530

    Update seeder to file and data to add categories

--- backend/data/products.js
+++ backend/data/products.js
@@ -5,7 +5,7 @@ const products = [
     description:
       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
     brand: 'Apple',
-    category: 'Electronics',
+    category: 'Apple',
     price: 89.99,
     countInStock: 3,
     rating: 0,
@@ -17,7 +17,7 @@ const products = [
     description:
       'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
     brand: 'Apple',
-    category: 'Electronics',
+    category: 'Apple',
     price: 599.99,
     countInStock: 10,
     rating: 0,
@@ -71,6 +71,6 @@ const products = [
     rating: 0,
     numReviews: 0,
   },
-]
+];
 
-export default products
+export default products;
